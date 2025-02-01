81f82124348b32e1c9f7831b418bba51b6809a0d
Md Zakir Hossain <miraz170221@gmail.com>
2025-02-01 21:25:38 +0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    course = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
---
+++ views.py
@@ -0,0 +1,42 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
+from .models import Student
+from .forms import StudentForm
+
+def student_list(request):
+    students = Student.objects.all()
+    return render(request, 'student_list.html', {'students': students})
+
+def student_create(request):
+    if request.method == "POST":
+        form = StudentForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Student added successfully!")
+            return redirect('student_list')
+    else:
+        form = StudentForm()
+    return render(request, 'student_form.html', {'form': form})
+
+def student_update(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    if request.method == "POST":
+        form = StudentForm(request.POST, instance=student)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Student updated successfully!")
+            return redirect('student_list')
+    else:
+        form = StudentForm(instance=student)
+    return render(request, 'student_form.html', {'form': form})
+
+def student_delete(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    student.delete()
+    messages.error(request, "Student record deleted!")
+    return redirect('student_list')
+
+def student_detail(request, pk):
+    student = get_object_or_404(Student, pk=pk)
+    return render(request, 'student_detail.html', {'student': student})
+
